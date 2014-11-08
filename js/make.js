fcabc760e1d3053d1c060220d8f016f5c77728e5
lanyanai <798484315@qq.com>
2014-11-08 14:19:18 +0800

    complete

--- js/make.js
+++ js/make.js
@@ -1,3 +1,41 @@
 /**
  * Created by zd on 2014/11/7.
  */
+$(function(){
+    $(".check-all").tap(function(){
+        var $self = $(this);
+        if($self.hasClass("checked")){
+            $self.removeClass("checked");
+            $(".day-check").removeClass("checked");
+            $(".pic-item").removeClass("checked");
+            $(".text-item").removeClass("checked");
+        }else{
+            $self.addClass("checked");
+            $(".day-check").addClass("checked");
+            $(".pic-item").addClass("checked");
+            $(".text-item").addClass("checked");
+        }
+
+        return false;
+    });
+    $(".day-check").tap(function(){
+        var $self = $(this);
+        var $picList = $self.parent().next();
+        if($self.hasClass("checked")){
+            $self.removeClass("checked");
+            $picList.children(".pic-item").removeClass("checked");
+            $picList.children(".text-item").removeClass("checked");
+        }else{
+            $self.addClass("checked");
+            $picList.children(".pic-item").addClass("checked");
+            $picList.children(".text-item").addClass("checked");
+        }
+    });
+
+    $(".pic-item").tap(function(){
+        $(this).toggleClass("checked");
+    });
+    $(".text-item").tap(function(){
+        $(this).toggleClass("checked");
+    });
+});
--- js/order.js
+++ js/order.js
@@ -1,3 +1,6 @@
 /**
  * Created by zd on 2014/11/7.
  */
+$(function(){
+    $("#areaSelect").citySelect({nodata:"none",required:false});
+});
